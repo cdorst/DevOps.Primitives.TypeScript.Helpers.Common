a7713c2e5edf2d10d22500bd882b9a4c7932cf38
Christopher Dorst <chris@dorst.email>
2018-04-06 11:39:45 -0500

    Add StatementLists helper

---
+++ DevOps.Primitives.TypeScript.Helpers.Common/StatementLists.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DevOps.Primitives.TypeScript.Helpers.Common
+{
+    public static class StatementLists
+    {
+        public static StatementList Create(params Statement[] statements)
+            => new StatementList(GetListItems(statements));
+
+        public static StatementList Create(params string[] statements)
+            => Create(statements.Select(s => new Statement(s)).ToArray());
+
+        private static List<StatementListAssociation> GetListItems(params Statement[] statements)
+            => statements.Select(statement => new StatementListAssociation(statement)).ToList();
+    }
+}
