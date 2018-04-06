2a22be030770eb4d72f70a113b987f214cae8ed4
Christopher Dorst <chris@dorst.email>
2018-04-06 11:38:58 -0500

    Add ImportStatmentLists helper

---
+++ DevOps.Primitives.TypeScript.Helpers.Common/ImportStatementLists.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DevOps.Primitives.TypeScript.Helpers.Common
+{
+    public static class ImportStatementLists
+    {
+        public static ImportStatementList ImportList(params ImportStatement[] imports)
+            => new ImportStatementList(GetListItems(imports));
+
+        private static List<ImportStatementListAssociation> GetListItems(ImportStatement[] imports)
+            => imports.Select(import => new ImportStatementListAssociation(import)).ToList();
+    }
+}
