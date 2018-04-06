956175588f463b5fa486406804c69c53614404d7
Christopher Dorst <chris@dorst.email>
2018-04-06 11:38:41 -0500

    Add ImportStatementEqualityComparer

---
+++ DevOps.Primitives.TypeScript.Helpers.Common/ImportStatementEqualityComparer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DevOps.Primitives.TypeScript.Helpers.Common
+{
+    public class ImportStatementEqualityComparer : EqualityComparer<ImportStatement>
+    {
+        public static ImportStatementEqualityComparer Instance
+            => new ImportStatementEqualityComparer();
+
+        public override bool Equals(ImportStatement x, ImportStatement y)
+            => x.ExportName.Name.Value.Equals(y.ExportName.Name.Value)
+            && x.ModuleName.Name.Value.Equals(y.ModuleName.Name.Value);
+
+        public override int GetHashCode(ImportStatement obj)
+            => obj.ModuleName.Name.Value.GetHashCode();
+    }
+}
