87c3c38c25c42e09fcccfb248580f3042d43eaf6
Christopher Dorst <chris@dorst.email>
2018-04-06 11:39:30 -0500

    Add ImportStatementListExtensions

---
+++ DevOps.Primitives.TypeScript.Helpers.Common/ImportStatementListExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Common.EntityFrameworkServices;
+using static DevOps.Primitives.TypeScript.Helpers.Common.ImportStatementEqualityComparer;
+
+namespace DevOps.Primitives.TypeScript.Helpers.Common
+{
+    public static class ImportStatementListExtensions
+    {
+        public static ImportStatementList Merge(this ImportStatementList instance, ImportStatementList given)
+            => instance.Merge<ImportStatementList, ImportStatement, ImportStatementListAssociation>(given, Instance);
+    }
+}
