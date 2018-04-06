cb13bc157a4962896a1dfc158e79171b75987056
Christopher Dorst <chris@dorst.email>
2018-04-06 11:40:21 -0500

    Add ArgumentLists helper

---
+++ DevOps.Primitives.TypeScript.Helpers.Common/ArgumentLists.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DevOps.Primitives.TypeScript.Helpers.Common
+{
+    public static class ArgumentLists
+    {
+        public static ArgumentList Create(params Argument[] arguments)
+            => new ArgumentList(GetListItems(arguments));
+
+        private static List<ArgumentListAssociation> GetListItems(params Argument[] arguments)
+            => arguments.Select(argument => new ArgumentListAssociation(argument)).ToList();
+    }
+}
