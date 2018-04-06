273f521d75bfef9f1e3a40a3dc8f79a00c088b87
Christopher Dorst <chris@dorst.email>
2018-04-06 11:39:16 -0500

    Add ImportStatements helper

---
+++ DevOps.Primitives.TypeScript.Helpers.Common/ImportStatements.cs
@@ -0,0 +1,8 @@
+﻿namespace DevOps.Primitives.TypeScript.Helpers.Common
+{
+    public static class ImportStatements
+    {
+        public static ImportStatement Import(string exportName, string moduleName)
+            => new ImportStatement(exportName, moduleName);
+    }
+}
