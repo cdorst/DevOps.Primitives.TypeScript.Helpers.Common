603729cc720a5f360f3ad53be944442ea27d8a98
Christopher Dorst <chris@dorst.email>
2018-04-06 11:40:30 -0500

    Add Arguments helper

---
+++ DevOps.Primitives.TypeScript.Helpers.Common/Arguments.cs
@@ -0,0 +1,8 @@
+﻿namespace DevOps.Primitives.TypeScript.Helpers.Common
+{
+    public static class Arguments
+    {
+        public static Argument Argument(string literal)
+            => new Argument(literal);
+    }
+}
