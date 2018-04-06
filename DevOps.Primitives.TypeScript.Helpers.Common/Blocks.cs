2fe80b19c4449d1bdab343233d4bfd10e7e6c05a
Christopher Dorst <chris@dorst.email>
2018-04-06 11:39:56 -0500

    Add Blocks helper

---
+++ DevOps.Primitives.TypeScript.Helpers.Common/Blocks.cs
@@ -0,0 +1,15 @@
+﻿namespace DevOps.Primitives.TypeScript.Helpers.Common
+{
+    public static class Blocks
+    {
+        public static Block Create(params Statement[] statements)
+            => new Block(
+                StatementLists.Create(statements));
+
+        public static Block Create(params string[] statements)
+            => new Block(
+                StatementLists.Create(statements));
+
+        public static Block Empty => new Block();
+    }
+}
